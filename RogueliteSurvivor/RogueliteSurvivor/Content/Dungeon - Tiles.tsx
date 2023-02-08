f5cbf9fd212835b6eac7ec59b5616ee3b4d63622
Micah Caunter <mcaunter12@gmail.com>
2023-02-07 20:05:37 -0500

    feat: added map and tileset that are displayed

---
+++ RogueliteSurvivor/RogueliteSurvivor/Content/Dungeon - Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Dungeon - Tiles" tilewidth="16" tileheight="16" tilecount="625" columns="25">
+ <image source="Tiles.png" trans="000000" width="400" height="400"/>
+</tileset>
--- RogueliteSurvivor/RogueliteSurvivor/Game1.cs
+++ RogueliteSurvivor/RogueliteSurvivor/Game1.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using TiledCS;
 
 namespace RogueliteSurvivor
 {
@@ -8,6 +13,14 @@ namespace RogueliteSurvivor
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private TiledMap map;
+        private Dictionary<int, TiledTileset> tilesets;
+        private Texture2D tilesetTexture;
+        private TiledLayer collisionLayer;
+
+        const int scaleFactor = 3;
+        private Matrix transformMatrix;
 
         public Game1()
         {
@@ -20,12 +33,18 @@ namespace RogueliteSurvivor
         {
             // TODO: Add your initialization logic here
 
+            transformMatrix = Matrix.CreateScale(scaleFactor, scaleFactor, 1f);
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            map = new TiledMap(Content.RootDirectory + "\\Demo.tmx");
+            tilesets = map.GetTiledTilesets(Content.RootDirectory + "/");
+            tilesetTexture = Content.Load<Texture2D>("Tiles");
 
             // TODO: use this.Content to load your game content here
         }
@@ -44,7 +63,51 @@ namespace RogueliteSurvivor
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp, transformMatrix: transformMatrix);  // Set samplerState to null to work with high res assets
+
+            var tileLayers = map.Layers.Where(x => x.type == TiledLayerType.TileLayer);
+
+            foreach (var layer in tileLayers)
+            {
+                for (var y = 0; y < layer.height; y++)
+                {
+                    for (var x = 0; x < layer.width; x++)
+                    {
+                        var index = (y * layer.width) + x; // Assuming the default render order is used which is from right to bottom
+                        var gid = layer.data[index]; // The tileset tile index
+                        var tileX = x * map.TileWidth;
+                        var tileY = y * map.TileHeight;
+
+                        // Gid 0 is used to tell there is no tile set
+                        if (gid == 0)
+                        {
+                            continue;
+                        }
+
+                        // Helper method to fetch the right TieldMapTileset instance
+                        // This is a connection object Tiled uses for linking the correct tileset to the gid value using the firstgid property
+                        var mapTileset = map.GetTiledMapTileset(gid);
+
+                        // Retrieve the actual tileset based on the firstgid property of the connection object we retrieved just now
+                        var tileset = tilesets[mapTileset.firstgid];
+
+                        // Use the connection object as well as the tileset to figure out the source rectangle
+                        var rect = map.GetSourceRect(mapTileset, tileset, gid);
+
+                        // Create destination and source rectangles
+                        var source = new Rectangle(rect.x, rect.y, rect.width, rect.height);
+                        var destination = new Rectangle(tileX, tileY, map.TileWidth, map.TileHeight);
+
+                        SpriteEffects effects = SpriteEffects.None;
+                        double rotation = 0f;
+
+                        // Render sprite at position tileX, tileY using the rect
+                        _spriteBatch.Draw(tilesetTexture, destination, source, Color.White, (float)rotation, Vector2.Zero, effects, 0);
+                    }
+                }
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
